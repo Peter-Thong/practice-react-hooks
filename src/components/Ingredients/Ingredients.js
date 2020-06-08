707a1e0b8b620decfaa7a6cd921d1500b0e5049b
Peter Nguyen <thong01677@gmail.com>
2020-06-08 09:50:24 -0400

    react hook useState

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
+import IngredientList from "./IngredientList";
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevState) => [
+      ...userIngredients,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
+  const removeIngredientHandler = (id) => {
+    setUserIngredients((prevState) =>
+      prevState.filter((ingredient) => ingredient.id !== id)
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
